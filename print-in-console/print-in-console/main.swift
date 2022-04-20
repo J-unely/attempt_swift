8a58585a12d74609869f99db08a1c784e1c7014e
J-unely <cdenen20@gmail.com>
2022-04-21 02:51:42 +0900

    dictionary, reversed

--- print-in-console/print-in-console/main.swift
+++ print-in-console/print-in-console/main.swift
@@ -29,3 +29,23 @@ func please(sweetLong heightA: Int, stickLong heightB: Int) {
 }
 
 please(sweetLong: 8, stickLong: 3)
+
+
+//시도: dictionary 형태로 별칭 대응
+var decoOrder:[String:String] = ["none":" ", "crunch":"*", "almond":"#"]
+let decos = decoOrder.keys
+let hereItis = decoOrder.values
+for decos in decos {
+    print(decos)
+}
+for hereItis in hereItis {
+    print(hereItis)
+}
+print("\n") //2줄 개행
+
+//시도: reverse와 reversed, 그리고 문자열 쪼갰다 붙이기
+let sth = "abcde"
+for char in sth.reversed() {
+    print(char, terminator: "")
+}
+print() //1줄 개행
