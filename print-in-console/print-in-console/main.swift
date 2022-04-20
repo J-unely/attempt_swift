c6a238332862745213e8ea76e09fb18b3d778a90
J-unely <cdenen20@gmail.com>
2022-04-21 02:41:15 +0900

    if문 간소화

--- print-in-console/print-in-console/main.swift
+++ print-in-console/print-in-console/main.swift
@@ -11,34 +11,21 @@ let deco = "#"
 let (basic, nude) = ("***","|0|")
 let spare = " "
 
-//let half = one + two + three
-//let whole = one + two + three + "\n" + one + two + three
-
-//print(half)
-//print()
-//print(whole)
-
 let halfOdd = deco + basic + spare
 let halfEven = spare + basic + deco
 
 //let (heightA, heightB) = (8, 3)
 
 func please(sweetLong heightA: Int, stickLong heightB: Int) {
-    if heightA % 2 == 0 {
-        for _ in 1...heightA / 2 {
-            print(halfOdd, halfEven, separator: "\n")
-        }
+    for _ in 1...heightA / 2 {
+        print(halfOdd, halfEven, separator: "\n")
     }
-    else if heightA % 2 != 0 {
-        for _ in 1...heightA / 2 {
-            print(halfOdd, halfEven, separator: "\n")
-        }
+    if heightA % 2 != 0 {
         print(halfOdd)
     }
     for _ in 1...heightB {
         print(" | | ")
     }
 }
-
 
 please(sweetLong: 8, stickLong: 3)
