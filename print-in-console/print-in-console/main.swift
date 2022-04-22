7bedca17b0373ccf7423af17979e100b893d9e03
J-unely <cdenen20@gmail.com>
2022-04-22 18:52:54 +0900

    성공초안 보완4 - 튜플형태 함수 파라미터

--- print-in-console/print-in-console/main.swift
+++ print-in-console/print-in-console/main.swift
@@ -16,23 +16,24 @@ var 타입: String
 var 옵션: String
 //var 데코: String
 
-func please(want 타입: String, with 옵션: String, sizeIs heightA: Int, stickIs heightB: Int){
+func please(want 타입: String, with 옵션: String, sizeOf length: (heightA: Int, heightB: Int)){
     let halfOdd = 옵션 + 타입 + spare
     let halfEven = spare + 타입 + 옵션
-    if heightA % 2 == 0 {
-        for _ in 1...heightA / 2 {
+    if length.heightA % 2 == 0 {
+        for _ in 1...length.heightA / 2 {
             print(halfOdd, halfEven, separator: "\n")
         }
     }
-    else if heightA % 2 != 0 {
-        for _ in 1...heightA / 2 {
+    else if length.heightA % 2 != 0 {
+        for _ in 1...length.heightA / 2 {
             print(halfOdd, halfEven, separator: "\n")
         }
         print(halfOdd)
     }
-    for _ in 1...heightB {
+    for _ in 1...length.heightB {
         print(" | | ")
     }
 }
 
-please(want: basic, with: crunch, sizeIs: 8, stickIs: 3)
+//please(want: basic, with: crunch, sizeIs: 8, stickIs: 3)
+please(want: basic, with: crunch, sizeOf: (heightA: 8, heightB: 3))
