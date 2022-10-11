b25b3a6a610818abd6fdb0d04ea650ee430f4f91
Junely <67406889+ueunli@users.noreply.github.com>
2022-10-11 20:17:21 +0900

    [unrated] Title: OX퀴즈, Time: 0.23 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+func solution(_ quiz:[String]) -> [String] {
+    
+    func check(_ exp: String) -> String {
+        let strArr = exp.components(separatedBy: " ")
+        var intArr = [Int]()
+        var isPlus = true
+        strArr.map { 
+            if let num = Int($0) {
+                intArr += [num]
+            } else {
+                switch $0 {
+                    case "+": isPlus = true
+                    case "-": isPlus = false
+                    default: break  //← "="인 경우
+                }
+            }
+        }
+        switch isPlus {
+            case true: guard intArr[0] + intArr[1] == intArr[2] else { return "X" }; return "O"
+            case false:guard intArr[0] - intArr[1] == intArr[2] else { return "X" }; return "O"
+        }
+    }
+    
+    return quiz.map { check($0) }
+    
+}
