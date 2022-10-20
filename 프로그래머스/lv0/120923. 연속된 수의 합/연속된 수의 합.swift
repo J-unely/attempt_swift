209abd7b485b1be6661e6c35b4fda2aee163caae
Junely <67406889+ueunli@users.noreply.github.com>
2022-10-20 19:56:56 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.11 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+func solution(_ num:Int, _ total:Int) -> [Int] {
+    let mid = total / num
+    let min = num % 2 == 1 ? mid - (num / 2) : mid - (num / 2 - 1)
+    let max = num % 2 == 1 ? mid + (num / 2) : mid + (num / 2)
+    return Array(min...max)
+}
